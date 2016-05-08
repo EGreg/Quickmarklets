5b29fdaa50268593b880a0b4557743b2007510b1
Greg Magarshak <greg@qbix.com>
2016-05-08 07:00:41 -0400

    Changed title

--- handlers/Q/response/title.php
+++ handlers/Q/response/title.php
@@ -2,11 +2,5 @@
 
 function Q_response_title()
 {
-	// The default title
-	$title = Q_Config::get('Q', 'app', basename(APP_DIR));
-	$action = Q_Dispatcher::uri()->action;
- 	if ($action) {
-		$title .= ": $action";
-	}
-	return $title;
+	return "Quickmarklets";
 }
